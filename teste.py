5d8a3653678dd2c4e4919381d013f10685e0a521
Mateus Amorim <mateusvidalamorim@gmail.com>
2024-07-29 15:30:04 -0300

    mdm

---
+++ teste.py
@@ -0,0 +1,30 @@
+import pyautogui 
+import time
+
+
+rep = 20
+
+# Simula um clique do mouse na posição especificada
+pyautogui.click(x=890, y=1050)
+    
+    # Aguarda um breve intervalo antes de simular o atalho Ctrl+V
+time
+
+for i in range(rep):
+
+    
+
+    
+
+    pyautogui.click(x=482, y=248)
+
+    pyautogui.hotkey('backspace')
+
+    pyautogui.click(x=291, y=243)
+
+    pyautogui.hotkey('backspace')
+
+    pyautogui.hotkey('\'')
+
+    pyautogui.hotkey('enter')
+
